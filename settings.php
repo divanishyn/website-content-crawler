adc2f10edd2b79781c6888d52ff0ecb9cb233596
Dmytro Ivanishyn <dima.zt@gmail.com>
2014-01-06 21:09:41 +0200

    Add settings file.

---
+++ settings.php
@@ -0,0 +1,7 @@
+<?php
+// ============= Access to database ======================
+define('DB_HOST',   'localhost');	// mysql server name
+define('DB_NAME',   'database_name');	// name of database
+define('DB_USER',   'user');		// user of database
+define('DB_PASS',   'password');	// and its password
+?>
